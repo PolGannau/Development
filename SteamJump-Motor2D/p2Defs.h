efb86f0883d52f02a5e637bbc01fffaf93c5bbdf
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2019-10-10 12:42:19 +0200

    Changing Project Configuration

--- SteamJump-Motor2D/p2Defs.h
+++ SteamJump-Motor2D/p2Defs.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef __P2DEFS_H__
+#define __P2DEFS_H__
+
+#include <stdio.h>
+
+// NULL just in case ---------------
+#ifdef NULL
+#undef NULL
+#endif // NULL
+#define NULL 0
+
+// Deletes a buffer
+
+
+
+#endif // !__P2DEFS_H__
