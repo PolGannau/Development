5f90e94887363cff6b6d4317015a2edd2ade491f
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2019-10-11 20:11:32 +0200

    Added utilities files
    Added:
    + p2Log
    + Creating p2DynArray

---
+++ SteamJump-Motor2D/p2DynArray.h
@@ -0,0 +1,9 @@
+// ---------------------------------------------------------
+// Array that resizes dynamically	------------------------
+// ---------------------------------------------------------
+#ifndef __P2DYNARRAY_H__
+#define __P2DYNARRAY_H__
+
+#include "p2Defs.h"
+
+#endif // !__P2DYNARRAY_H__
