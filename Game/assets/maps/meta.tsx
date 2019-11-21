fc779a8a68e33a708d2841087aa163ac7c43362a
Pol Gannau Coma <36154511+PolGannau@users.noreply.github.com>
2019-11-21 13:46:22 +0100

    Map
    Navigation added and some colliders, more colliders to come.

--- Game/assets/maps/meta.tsx
+++ Game/assets/maps/meta.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="meta" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="4" columns="2">
- <image source="../../../../../../Desktop/CITM_2_SpookySkeleton_PlatformGame-master/Code/Game/textures/meta.png" width="67" height="68"/>
+ <image source="meta.png" width="67" height="68"/>
 </tileset>
